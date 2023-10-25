c4aed9c168d88665d9cb3e4452e579f7a572441c
Venus741 <124938461+Venus741@users.noreply.github.com>
2023-10-25 10:17:14 -0300

    first commit

---
+++ src/cadastro.js
@@ -0,0 +1,2 @@
+const inicial = () => window.location.assign('../fonts/index.html'); //Voltar à página index ao clicar no título
+const concluir = () => window.alert('Botão funcionando') // Funcionamento do botão
